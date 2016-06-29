8b90b7402d42955309671bd03644c12c3b482b59
scynetpro <5821262@gmail.com>
2016-06-29 11:36:58 +0300

    fix

--- resources/views/general/auth/social.blade.php
+++ resources/views/general/auth/social.blade.php
@@ -1,6 +1,19 @@
 @push('scripts')
 <script src="//ulogin.ru/js/ulogin.js"></script>
+<script>
+    function login_from_social (token) {
+        var form = document.createElement('form');
+        form.method = 'POST';
+        form.action = '{{url('/ulogin')}}';
 
+        var uToken = document.createElement('input');
+        uToken.name = 'uToken';
+        uToken.value = token;
+
+        form.appendChild(uToken);
+        form.submit();
+    }
+</script>
 @endpush
 <div data-ulogin="display=buttons;fields=first_name,last_name,email,photo_big;providers=vk,facebook;hidden=;redirect_uri=;callback=login_from_social">
     <div data-uloginbutton="vkontakte" class="reg-social__item login-social-button login-social-button_vk login-social-button_big">Войти через Вконтакте</div>
